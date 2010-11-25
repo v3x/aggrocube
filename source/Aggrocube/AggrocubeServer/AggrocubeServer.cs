004666b917debf8b3e6a5451caf43b4aa30a0cef
Antono Miĉjo Petro <v3x@nowhere.here>
2010-11-24 22:20:50 -0500

    Created branch to test networking functionality.

--- source/Aggrocube/AggrocubeServer/AggrocubeServer.cs
+++ source/Aggrocube/AggrocubeServer/AggrocubeServer.cs
@@ -1,19 +1,63 @@
 using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading;
 namespace AggrocubeServer
 {
 	public class AggrocubeServer
-	{		
-		public AggrocubeServer ()
+	{
+		private TcpListener tcpListener;
+		private Thread listenThread;
+
+		public AggrocubeServer(int listenPort)
 		{
+			this.tcpListener = new TcpListener(IPAddress.Any, listenPort);
+			this.listenThread = new Thread(new ThreadStart(ListenForClients));
+			this.listenThread.Start();
+		}
+
+		private void ListenForClients()
+		{
+			this.tcpListener.Start();
+			while(true) {
+				TcpClient tcpClient = this.tcpListener.AcceptTcpClient();
+				Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+				clientThread.Start(tcpClient);
+			}
+		}
+
+		private void HandleClientComm (object client)
+		{
+			TcpClient tcpClient = (TcpClient)client;
+			NetworkStream clientStream = tcpClient.GetStream ();
+			
+			byte[] message = new byte[4096];
+			int bytesRead;
+			
+			while (true) {
+				bytesRead = 0;
+				try {
+					bytesRead = clientStream.Read (message, 0, 4096);
+				} catch {
+					break;
+				}
+				
+				if (bytesRead == 0) {
+					break;
+				}
+				
+				UTF8Encoding encoder = new UTF8Encoding();
+				System.Diagnostics.Debug.WriteLine (encoder.GetString (message, 0, bytesRead));
+			}
+			
+			tcpClient.Close ();
 		}
 
 		public static void Main (string[] args)
 		{
-			new AggrocubeServer ();
+			new AggrocubeServer (9001);
 		}
 	}
-	
-	
 }
 
